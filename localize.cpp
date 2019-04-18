9a1dbddbcf9c87e98a92927aa7e21f7a87368a79
joseglauberbo <jose.oliveira@ccc.ufcg.edu.br>
2019-04-18 17:04:17 -0300

    Alteração no arquivo localize.cpp

--- localize.cpp
+++ localize.cpp
@@ -9,37 +9,51 @@
 #include <ctime>
 
 
+using namespace std;
 void apresentacao(){
 	while(true){
-		
-		
-	}
+	
+    	cout << "██████╗ ███████╗███╗   ███╗    ██╗   ██╗██╗███╗   ██╗██████╗  ██████╗ "<<endl;
+        cout << "██╔══██╗██╔════╝████╗ ████║    ██║   ██║██║████╗  ██║██╔══██╗██╔═══██╗"<<endl;
+        cout << "██████╔╝█████╗  ██╔████╔██║    ██║   ██║██║██╔██╗ ██║██║  ██║██║   ██║"<<endl;
+        cout << "██╔══██╗██╔══╝  ██║╚██╔╝██║    ╚██╗ ██╔╝██║██║╚██╗██║██║  ██║██║   ██║"<<endl;
+        cout << "██████╔╝███████╗██║ ╚═╝ ██║     ╚████╔╝ ██║██║ ╚████║██████╔╝╚██████╔╝"<<endl;
+	    cout << "╚═════╝ ╚══════╝╚═╝     ╚═╝      ╚═══╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ "<<endl;
+        cout << "À LOCALIZE CAR"<<endl;
+        cout<<" "<<endl;
+        cout << "COMO VOCÊ DESEJA SEGUIR?"<<endl;
+        cout << "(1) PESQUISAR/ALUGAR"<<endl;
+        cout << "(2) DEVOLVER"<<endl;
+        cout << "(3) SAIR" <<endl;
+    	int opcao;
+
+        cin >> opcao;
+
+        if (opcao == 3)
+        {
+            break;
+        }
+        else if (opcao == 1)
+        {
+            break;
+        }
+        else if (opcao == 2)
+        {
+           break;
+        }
+        else
+        {
+            cout << "SELECIONE UMA OPÇAO VALIDA!"<<endl;
+            cout<<" "<<endl;
+        }
+    }
 	
 }
 
 
-
-
-
-        
-
-
-
 int main()
 {
-		std::cout << "██████╗ ███████╗███╗   ███╗    ██╗   ██╗██╗███╗   ██╗██████╗  ██████╗ \n";
-        std::cout << "██╔══██╗██╔════╝████╗ ████║    ██║   ██║██║████╗  ██║██╔══██╗██╔═══██╗\n";
-        std::cout << "██████╔╝█████╗  ██╔████╔██║    ██║   ██║██║██╔██╗ ██║██║  ██║██║   ██║\n";
-        std::cout << "██╔══██╗██╔══╝  ██║╚██╔╝██║    ╚██╗ ██╔╝██║██║╚██╗██║██║  ██║██║   ██║\n";
-        std::cout << "██████╔╝███████╗██║ ╚═╝ ██║     ╚████╔╝ ██║██║ ╚████║██████╔╝╚██████╔╝\n";
-		std::cout << "╚═════╝ ╚══════╝╚═╝     ╚═╝      ╚═══╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ \n";
-        std::cout << "À LOCALIZE CAR\n";
-        std::cout << "COMO VOCÊ DESEJA SEGUIR?\n";
-        std::cout << "(1) PESQUISAR/ALUGAR\n";
-        std::cout << "(2) DEVOLVER\n";
-        std::cout << "(3) SAIR\n";
-
-
+ apresentacao();
 
 return 0;
 }
